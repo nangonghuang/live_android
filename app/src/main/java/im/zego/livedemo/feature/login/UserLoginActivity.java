ef3742f6cab551ab5a0efd71889c02ee545f3346
rocket_wang <rocketwang@zego.im>
2021-12-22 17:07:03 +0800

    rewrite user login & settings activity with view-binding

--- app/src/main/java/im/zego/livedemo/feature/login/UserLoginActivity.java
+++ app/src/main/java/im/zego/livedemo/feature/login/UserLoginActivity.java
@@ -1,9 +1,10 @@
 package im.zego.livedemo.feature.login;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
-import android.widget.Button;
-import android.widget.EditText;
 
 import com.blankj.utilcode.util.DeviceUtils;
 import com.blankj.utilcode.util.StringUtils;
@@ -20,45 +21,52 @@ import im.zego.live.model.ZegoUserInfo;
 import im.zego.live.util.TokenServerAssistant;
 import im.zego.livedemo.KeyCenter;
 import im.zego.livedemo.R;
-import im.zego.livedemo.feature.BaseActivity;
-import im.zego.livedemo.helper.PermissionHelper;
+import im.zego.livedemo.base.BaseActivity;
+import im.zego.livedemo.databinding.ActivityUserLoginBinding;
+import im.zego.livedemo.feature.room.RoomListActivity;
 
-public class UserLoginActivity extends BaseActivity {
-    private EditText etUserId;
-    private EditText etUserName;
-    private Button btnLogin;
+public class UserLoginActivity extends BaseActivity<ActivityUserLoginBinding> {
+
+    public static void start(Context context) {
+        Intent intent = new Intent(context, UserLoginActivity.class);
+        context.startActivity(intent);
+    }
 
     // login info
     private String userID;
     private String userName;
 
+    @SuppressLint("ResourceType")
+    @Override
+    protected int getStatusBarColor() {
+        return R.color.login_dark_bg;
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_user_login);
-        initUI();
-        PermissionHelper.requestRecordAudio(this, null);
+        initData();
+        initListener();
     }
 
-    private void initUI() {
-        etUserId = findViewById(R.id.et_user_id);
-        etUserName = findViewById(R.id.et_user_name);
-        btnLogin = findViewById(R.id.btn_login);
+    private void initData() {
+        binding.etUserId.setText(DeviceUtils.getManufacturer() + (int) (100 + Math.random() * 900));
+    }
 
-        btnLogin.setOnClickListener(v -> {
-            userID = etUserId.getText().toString();
-            userName = etUserName.getText().toString();
+    private void initListener() {
+        binding.btnLogin.setOnClickListener(v -> {
+            userID = binding.etUserId.getText().toString();
+            userName = binding.etUserName.getText().toString();
             if (TextUtils.isEmpty(userName)) {
                 userName = userID;
             }
             String regEx = "^[a-zA-Z\\d]+$";
             Pattern p = Pattern.compile(regEx);
-            Matcher m = p.matcher(etUserId.getText().toString());
+            Matcher m = p.matcher(binding.etUserId.getText().toString());
             if (!m.matches()) {
                 ToastUtils.showShort(R.string.toast_user_id_error);
                 return;
             }
-
 
             ZegoUserInfo user = new ZegoUserInfo();
             if (!(TextUtils.isEmpty(userID))) {
@@ -69,6 +77,7 @@ public class UserLoginActivity extends BaseActivity {
                     String token = TokenServerAssistant.generateToken(KeyCenter.appID(), userID, KeyCenter.appZIMServerSecret(), 60 * 60 * 24).data;
                     ZegoRoomManager.getInstance().userService.login(user, token, errorCode -> {
                         if (errorCode == ZegoRoomErrorCode.SUCCESS) {
+                            RoomListActivity.start(this);
                         } else {
                             ToastUtils.showShort(StringUtils.getString(R.string.toast_login_fail, errorCode));
                         }
@@ -80,7 +89,5 @@ public class UserLoginActivity extends BaseActivity {
                 ToastUtils.showShort(StringUtils.getString(R.string.toast_userid_login_fail));
             }
         });
-
-        etUserId.setText(DeviceUtils.getManufacturer() + (int) (100 + Math.random() * 900));
     }
 }
