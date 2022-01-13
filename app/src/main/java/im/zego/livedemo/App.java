e7cec43287d426e3f794889ecc483acbf3959507
rock <rockdev@xx.com>
2022-01-13 15:58:49 +0800

    add crash handler to catch crash information

--- app/src/main/java/im/zego/livedemo/App.java
+++ app/src/main/java/im/zego/livedemo/App.java
@@ -5,6 +5,7 @@ import com.blankj.utilcode.util.LogUtils;
 import com.blankj.utilcode.util.Utils;
 import im.zego.live.ZegoRoomManager;
 import im.zego.livedemo.helper.AuthInfoManager;
+import im.zego.livedemo.helper.CrashHandler;
 import im.zego.livedemo.helper.SoundEffectsHelper;
 
 public class App extends Application {
@@ -12,6 +13,7 @@ public class App extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        CrashHandler.getInstance().init(this);
         Utils.init(this);
         LogUtils.getConfig().setLogHeadSwitch(false);
         LogUtils.getConfig().setBorderSwitch(false);
