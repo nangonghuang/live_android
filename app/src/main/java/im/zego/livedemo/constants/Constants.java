c749d99fb962817d121bf0248e7f1adc31ed9000
rocket_wang <rocketwang@zego.im>
2022-01-18 19:20:10 +0800

    url to formal environment

--- app/src/main/java/im/zego/livedemo/constants/Constants.java
+++ app/src/main/java/im/zego/livedemo/constants/Constants.java
@@ -6,8 +6,8 @@ package im.zego.livedemo.constants;
 public class Constants {
     public static final String URL_GET_MORE = "https://www.zegocloud.com/";
     public static final String URL_SIGN_UP = "https://www.zegocloud.com/account/signup";
-    public static final String URL_CONTACT_US = "http://192.168.100.210:8081/contact";
-    public static final String URL_DOWNLOAD = "http://192.168.100.210:8081/live-join";
+    public static final String URL_CONTACT_US = "https://global.zegocloud.com/contact";
+    public static final String URL_DOWNLOAD = "https://global.zegocloud.com/live-join";
 
     public static final String URL_TERMS_OF_SERVICE = "https://www.zegocloud.com/policy?index=1";
     public static final String URL_PRIVACY_POLICY = "https://www.zegocloud.com/policy?index=0";
