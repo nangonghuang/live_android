07ed48cee508342e54ed9cc5bd4dcf972cdff70e
rocket_wang <rocketwang@zego.im>
2022-01-19 15:14:38 +0800

    beautify welcome entrance photo

--- app/src/main/java/im/zego/livedemo/feature/login/WelcomeActivity.java
+++ app/src/main/java/im/zego/livedemo/feature/login/WelcomeActivity.java
@@ -2,6 +2,11 @@ package im.zego.livedemo.feature.login;
 
 import android.os.Bundle;
 
+import com.blankj.utilcode.util.LanguageUtils;
+
+import java.util.Locale;
+
+import im.zego.livedemo.R;
 import im.zego.livedemo.base.BaseActivity;
 import im.zego.livedemo.constants.Constants;
 import im.zego.livedemo.databinding.ActivityWelcomeBinding;
@@ -12,10 +17,15 @@ public class WelcomeActivity extends BaseActivity<ActivityWelcomeBinding> {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        initListener();
+        initView();
     }
 
-    private void initListener() {
+    private void initView() {
+        if (LanguageUtils.getSystemLanguage().getLanguage().contains(Locale.CHINESE.getLanguage())) {
+            binding.layoutLiveShow.setBackgroundResource(R.drawable.icon_live_show_entrance_zh);
+        } else {
+            binding.layoutLiveShow.setBackgroundResource(R.drawable.icon_live_show_entrance);
+        }
         binding.layoutLiveShow.setOnClickListener(v -> UserLoginActivity.start(this));
         binding.tvGetMore.setOnClickListener(v -> WebViewActivity.start(this, Constants.URL_GET_MORE));
         binding.flSignUp.setOnClickListener(v -> WebViewActivity.start(this, Constants.URL_SIGN_UP));
